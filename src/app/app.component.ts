8844bef22176ccd7a46368cb3ab3683e9f1c5f84
ahmad-zahir88 <91833149+ahmad-zahir88@users.noreply.github.com>
2024-12-31 19:33:49 +1100

    feat: component projection from selected component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component, Type } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ComponentSelectorComponent } from './component-selector/component-selector.component';
+import { ComponentPortal, PortalModule } from '@angular/cdk/portal';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, ComponentSelectorComponent],
+  imports: [
+    MatToolbarModule,
+    ComponentSelectorComponent,
+    PortalModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular Material Styling Override Demo';
+  selectedComponentPortal?: ComponentPortal<any>;
 
-  onComponentSelectedEvent(event: Type<Component> | undefined) {
-    // @TODO: Add functionality to render the component
+  onComponentSelected<T extends Component>(event: Type<T> | undefined) {
+    this.selectedComponentPortal = event
+      ? new ComponentPortal(event)
+      : undefined;
   }
 }
--- src/app/component-selector/component-selector.component.ts
+++ src/app/component-selector/component-selector.component.ts
@@ -1,5 +1,5 @@
-import { Component, model, output, Type } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, output, Type } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
@@ -10,12 +10,32 @@ interface ComponentOption {
 
 @Component({
   selector: 'app-component-selector',
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './component-selector.component.html',
   styleUrl: './component-selector.component.scss',
 })
 export class ComponentSelectorComponent {
   selected = output<Type<Component>>();
-  selectedComponent = model<Type<Component>>();
+
+  componentForm: FormGroup;
+
   componentOptions: ComponentOption[] = [];
+
+  constructor(private _fb: FormBuilder) {
+    this.componentForm = this._fb.group<{
+      component: Type<Component> | null;
+    }>({ component: null });
+
+    this._autoEmitComponentChange();
+    this._populateComponentOptions();
+  }
+
+  private _autoEmitComponentChange() {
+    this.componentForm.get('component')?.valueChanges.subscribe((value) => {
+      this.selected.emit(value ?? undefined);
+    });
+  }
+
+  private _populateComponentOptions() {
+  }
 }
