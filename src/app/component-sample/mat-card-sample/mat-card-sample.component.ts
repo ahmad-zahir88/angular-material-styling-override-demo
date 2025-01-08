029cb3ce01b9045114c1a494aad95d5188c5c5ad
ahmad-zahir88 <91833149+ahmad-zahir88@users.noreply.github.com>
2025-01-08 16:08:20 +1100

    feat: add dimension-related properties to MatCardSample

--- src/app/component-sample/mat-card-sample/mat-card-sample.component.ts
+++ src/app/component-sample/mat-card-sample/mat-card-sample.component.ts
@@ -12,8 +12,12 @@ import { ComponentSampleBase } from '../component-sample';
 export class MatCardSampleComponent extends ComponentSampleBase {
   overrideNames = [
     'subtitle-text-color',
+    'title-text-line-height',
+    'subtitle-text-line-height',
+    'subtitle-text-size',
     'elevated-container-color',
     'outlined-container-color',
+    'outlined-outline-width',
     'outlined-outline-color',
   ];
 }
--- src/override-adder/override.ts
+++ src/override-adder/override.ts
@@ -5,6 +5,10 @@ export const OverrideNameToTypeMap = {
   'elevated-container-color': 'color',
   'outlined-container-color': 'color',
   'outlined-outline-color': 'color',
+  'title-text-line-height': 'dimension',
+  'subtitle-text-line-height': 'dimension',
+  'subtitle-text-size': 'dimension',
+  'outlined-outline-width': 'dimension',
 };
 
 export type OverrideName = keyof typeof OverrideNameToTypeMap;
